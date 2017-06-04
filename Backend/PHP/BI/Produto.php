01dff2b043fcb57194429d748ae120dbfdbccd3f
igorayelloborgesdev <igorayelloborgesdev@gmail.com>
2017-06-04 16:48:32 -0300

    Produto Atualizar

--- Backend/PHP/BI/Produto.php
+++ Backend/PHP/BI/Produto.php
@@ -63,29 +63,110 @@
         /*Exclui um produto*/
         public function Excluir($usuarioID, $ID, $CategoriaID)
         {
-            if($CategoriaID == null || $CategoriaID == 0)
-                return 'Por favor selecione uma categoria';
-            if($ID == null || $ID == 0)
-                return 'ID do produto inválido';
-            $usuarioBI = new UsuarioBI();
-            if(!$usuarioBI->ValidarUsuarioPorID($usuarioID))
-                return 'Usuário não encontrado';
-            $ProdutoCategoriaBI = new ProdutoCategoriaBI();
-            if(!$ProdutoCategoriaBI->ValidarCategoriaProdutoPorID($CategoriaID))
-                return 'Categoria não registrada';
-            //Exclui o produto
-            $produtoDB = new ProdutoDB();
-            $colunasProduto = Util::MontarStringComArray($produtoDB->getColunas());
-            $produtoDB->Apagar(' WHERE ID=' . $ID . ' AND CategoriaID=' . $CategoriaID);
-            //Verifica se o produto foi excluído
-            if(count(json_decode($produtoDB->Consultar($colunasProduto, ' WHERE ID=' . $ID . ' AND CategoriaID=' . $CategoriaID), true)) == 0)
-                return 'Produto excluído com sucesso';
-            else
-                return 'Não foi possível excluir o produto';
+            try 
+            {
+                if($CategoriaID == null || $CategoriaID == 0)
+                    return 'Por favor selecione uma categoria';
+                if($ID == null || $ID == 0)
+                    return 'ID do produto inválido';
+                $usuarioBI = new UsuarioBI();
+                if(!$usuarioBI->ValidarUsuarioPorID($usuarioID))
+                    return 'Usuário não encontrado';
+                $ProdutoCategoriaBI = new ProdutoCategoriaBI();
+                if(!$ProdutoCategoriaBI->ValidarCategoriaProdutoPorID($CategoriaID))
+                    return 'Categoria não registrada';
+                //Exclui o produto
+                $produtoDB = new ProdutoDB();
+                $colunasProduto = Util::MontarStringComArray($produtoDB->getColunas());
+                $produtoDB->Apagar(' WHERE ID=' . $ID . ' AND CategoriaID=' . $CategoriaID);
+                //Verifica se o produto foi excluído
+                if(count(json_decode($produtoDB->Consultar($colunasProduto, ' WHERE ID=' . $ID . ' AND CategoriaID=' . $CategoriaID), true)) == 0)
+                    return 'Produto excluído com sucesso';
+                else
+                    return 'Não foi possível excluir o produto';
+            }catch (Exception $e) {
+                return "ERRO: " .  $e->getMessage();
+            }
+        }
+        //Atualiza um produto
+        public function Atualizar($usuarioID, $ID, $Nome, $CaminhoImagem, $Descricao, $CategoriaID, $Ativo, $imagemExtensao = '')
+        {
+            try 
+            {
+                $usuarioBI = new UsuarioBI();
+                if(!$usuarioBI->ValidarUsuarioPorID($usuarioID))
+                    return 'Usuário não encontrado';
+                 if($ID == null || $ID == 0)
+                    return 'ID do produto inválido';
+                if($Nome == null)
+                    return 'Nome do produto inválido';
+                if($CaminhoImagem == null)
+                    return 'Imagem do produto inválida';
+                if($Descricao == null)
+                    return 'Descrição do produto inválida';
+                 if($CategoriaID == null || $CategoriaID == 0)
+                    return 'Por favor selecione uma categoria'; 
+                $ProdutoCategoriaBI = new ProdutoCategoriaBI();
+                if(!$ProdutoCategoriaBI->ValidarCategoriaProdutoPorID($CategoriaID))
+                    return 'Categoria não registrada';
+                $produtoDB = new ProdutoDB();
+                $colunasRemover = $produtoDB->getColunas();//Recebe as coluna do banco
+                $colunasProduto = Util::MontarStringComArray($produtoDB->getColunas());
+                $nomeSemEspacos = preg_replace('/\s+/', '', $Nome);
+                $nomeSemEspacos = preg_replace('/[^A-Za-z0-9\-]/', '', $nomeSemEspacos);
+                $verificarImagemExiste = $this->caminhoImagemServidorSalvar . "\\" . $nomeSemEspacos . "." . $imagemExtensao;
+                $imgExiste = "";
+                if(file_exists($verificarImagemExiste)) 
+                    $imgExiste = "1";
+                $caminhoImagemfinal = "'" . $this->caminhoImagemServidor . "\\\\" . $nomeSemEspacos . $imgExiste . "." . $imagemExtensao . "'";
+                $caminhoImagemfinalSalvar = $this->caminhoImagemServidorSalvar . "\\\\" . $nomeSemEspacos . $imgExiste . "." . $imagemExtensao;
+                array_splice($colunasRemover, 0, 1);//Retira a coluna ID
+                $valorAtualizar = array(
+                    $colunasRemover[0] => $Nome,
+                    $colunasRemover[1] => $caminhoImagemfinal,
+                    $colunasRemover[2] => $Descricao,
+                    $colunasRemover[3] => $CategoriaID,
+                    $colunasRemover[4] => $Ativo
+                );
+                $colunasInserir = Util::MontarStringComDictionaryUpdate($valorAtualizar);
+                //Atualiza imagem no banco
+                $nomeSemEspacos = preg_replace('/\s+/', '', $Nome);
+                $nomeSemEspacos = preg_replace('/[^A-Za-z0-9\-]/', '', $nomeSemEspacos);
+                $verificarImagemExiste = $this->caminhoImagemServidorSalvar . "\\" . $nomeSemEspacos . "." . $imagemExtensao;
+                $imgExiste = "";
+                if(file_exists($verificarImagemExiste)) 
+                    $imgExiste = "1";
+                $caminhoImagemfinal = "'" . $this->caminhoImagemServidor . "\\" . $nomeSemEspacos . $imgExiste . "." . $imagemExtensao . "'";
+                $caminhoImagemfinalSalvar = $this->caminhoImagemServidorSalvar . "\\" . $nomeSemEspacos . $imgExiste . "." . $imagemExtensao;
+                //Verifica se uma imagem foi carregada pelo usuário
+                if($CaminhoImagem != '' && $CaminhoImagem != null && $imagemExtensao != '' && $imagemExtensao != null)
+                    copy($CaminhoImagem , $caminhoImagemfinalSalvar);
+                //Verifica se a imagem foi salva no banco
+                if (!file_exists($caminhoImagemfinalSalvar)) 
+                    return 'Não foi possível salvar a imagem em nosso servidor';
+                //Deleta imagem antiga
+                $colunasProduto = Util::MontarStringComArray($produtoDB->getColunas());
+                $json = json_decode($produtoDB->Consultar($colunasProduto,  ' WHERE ID='. $ID), true);
+                $imagemAntgaExcluir = $json[0]['CaminhoImagem'];
+                unlink($imagemAntgaExcluir);
+                //Verifica se a imagem foi deletada
+                if(file_exists($imagemAntgaExcluir)) 
+                    return "Não foi possível substituir a imagem";
+                //Atualiza os dados cadastrados no banco
+                $produtoDB->Atualizar($colunasInserir, ' WHERE ID='. $ID);
+                //Verifica se o produto foi atualizado
+                if(count(json_decode($produtoDB->Consultar($colunasProduto,  ' WHERE Nome=' . $Nome . ' AND ID='. $ID . ' AND CategoriaID='. $CategoriaID . ' AND Ativo='.$Ativo), true)) == 1)
+                    return 'Produto ' . $Nome . ' atualizado com sucesso';
+                else
+                    return 'Não foi possível atualizar o produto ' . $Nome;
+            }catch (Exception $e) {
+                return "ERRO: " .  $e->getMessage();
+            }
         }
     }
     
     $a = new ProdutoBI();
-    $a->Inserir(3, "Triste Visionário", 9, 'C:\Users\IGOR\Downloads\imagem.jpg', "jpg");
+    //$a->Inserir(3, "Triste Visionário", 9, 'C:\Users\IGOR\Downloads\imagem.jpg', "jpg");
     //$a->Excluir(3, 91, 9);
+    $a->Atualizar(3, 92, "'Novo Nome'", 'C:\\\\xampp\\\htdocs\\\Teste_ControleEstoque\\\Backend\\\PHP\\\Img\\\TristeVisionrio.jpg', "'Nova descrição'", 9, 1, "jpg");
 ?>
